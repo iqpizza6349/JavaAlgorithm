ec19f0dd15808c2d74bb99d12181dbf9bc49671f
iqpizza6349 <iqpizza6349@gmail.com>
2023-01-10 09:49:44 +0900

    :tada: :: 알고리즘 공부 시작

---
+++ README.md
@@ -0,0 +1,9 @@
+## Java-Algorithm
+
+[알고리즘 Doit!](http://www.yes24.com/Product/Goods/60547893)을 기반으로 나오는 
+문제들을 직접 풀며, 알고리즘의 접근성을 기르기 위한 레포지토리입니다.
+
+
+
+
+
---
+++ src/me/iqpizza6349/javalgorithm/Main.java
@@ -0,0 +1,7 @@
+package me.iqpizza6349.javalgorithm;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
